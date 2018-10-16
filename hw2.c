c888c78b6a0247f31e65c5b6ba9be9458a8f1bfd
Frank Liang <fengnian.liang@stonybrook.edu>
2018-10-16 11:01:20 -0400

    update 2

--- hw2.c
+++ hw2.c
@@ -6,8 +6,8 @@ int main(int argc,char*argv[]){
     int message [messageSize];
     double total_my_bcast_time = 0.0;
     double total_mpi_bcast_time = 0.0;
-    for( i = 0; i<100; i = i+1){
-        message[i] = 1;
+    for( i = 0; i<messageSize; i = i+1){
+        message[i] =i;
     }
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &coreId);
@@ -32,7 +32,7 @@ int main(int argc,char*argv[]){
      //my_bcast
     MPI_Barrier(MPI_COMM_WORLD);
     total_my_bcast_time -= MPI_Wtime();
-     if(coreId == 0){
+    if(coreId == 0){
          int count;
          for(count = 1; count<totalCore, count = count +1){
             MPI_Isend(message, messageSize, MPI_INT, count, 0, MPI_COMM_WORLD, &request);
@@ -40,8 +40,8 @@ int main(int argc,char*argv[]){
         printf("message broadcasted from core %d \n", coreId);
     }
     else{
-        int recBuffer [messageSize];
-        MPI_Irecv(recBuffer, messageSize, MPI_INT, 0, 0, MPI_COMM_WORLD, &request);
+        int anotherBuffer [messageSize];
+        MPI_Irecv(anotherBuffer, messageSize, MPI_INT, 0, 0, MPI_COMM_WORLD, &request);
         printf("%d numbers received from core 0 by core %d \n", messageSize, coreId);   
     }
     MPI_Barrier(MPI_COMM_WORLD);

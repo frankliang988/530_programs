748ed8f0f5259f914776141ea80d0c0e9aa1536c
Frank Liang <fengnian.liang@stonybrook.edu>
2018-10-16 11:38:44 -0400

    UPDATE 4

--- hw2.c
+++ hw2.c
@@ -3,6 +3,7 @@
 int main(int argc,char*argv[]){
     int coreId, totalCore, i;
     int messageSize = 100;
+    int trial = 10;
     int message [messageSize];
     double total_my_bcast_time = 0.0;
     double total_mpi_bcast_time = 0.0;
@@ -13,37 +14,40 @@ int main(int argc,char*argv[]){
     MPI_Comm_rank(MPI_COMM_WORLD, &coreId);
     MPI_Comm_size(MPI_COMM_WORLD, &totalCore);
     MPI_Request request;
+    int times;
+    int anotherBuffer [messageSize];
+    for(times = 0; times < trial, times++){
      //my_bcast
     MPI_Barrier(MPI_COMM_WORLD);
     total_my_bcast_time -= MPI_Wtime();
     if(coreId == 0){
          int count;
-         for(count = 1; count<totalCore, count = count +1){
-            MPI_Isend(message, messageSize, MPI_INT, count, 0, MPI_COMM_WORLD, &request);
+         for(count = 1; count<totalCore; count = count +1){
+            MPI_Send(message, messageSize, MPI_INT, count, 0, MPI_COMM_WORLD);
          }
-        printf("message broadcasted from core %d \n", coreId);
+        printf("%d message broadcasted from core %d using Send \n", messageSize, coreId);
     }
     else{
-        int anotherBuffer [messageSize];
-        MPI_Irecv(anotherBuffer, messageSize, MPI_INT, 0, 0, MPI_COMM_WORLD, &request);
-        printf("%d numbers received from core 0 by core %d \n", messageSize, coreId);   
+        MPI_Recv(anotherBuffer, messageSize, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("%d numbers received from core 0 by core %d using Recv\n", messageSize, coreId);  
+        anotherBuffer = NULL; 
     }
     MPI_Barrier(MPI_COMM_WORLD);
     total_my_bcast_time += MPI_Wtime();
 
     //MPI_Bcast 
-    MPI_Barrier(MPI_COMM_WORLD);
-    total_mpi_bcast_time -= MPI_Wtime();
+     MPI_Barrier(MPI_COMM_WORLD);
+     total_mpi_bcast_time -= MPI_Wtime();
      MPI_Bcast(message, messageSize, MPI_INT, 0, MPI_COMM_WORLD);
-     printf("100 numbers received from core 0 by core %d \n", coreId);   
+     printf("%d numbers received from core 0 by core usng Bcast %d \n", messageSize, coreId);   
      MPI_Barrier(MPI_COMM_WORLD);
      total_mpi_bcast_time += MPI_Wtime();
-     
+    }
 
      if (coreId == 1) {
      printf("Data size = %d\n", messageSize);
-     printf("MPI_Bcast time = %lf\n", total_mpi_bcast_time);
-     printf("my bcast time = %lf\n", total_my_bcast_time);
+     printf("Average MPI_Bcast time = %lf\n", total_mpi_bcast_time/trial);
+     printf("Average my bcast time = %lf\n", total_my_bcast_time/trial);
   }
     MPI_Finalize();
 }

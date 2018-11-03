251a0055e7ca1075bfcadb2b47a8580e31c810f6
Frank Liang <fengnian.liang@stonybrook.edu>
2018-11-03 15:57:01 -0400

    hw3

---
+++ MatMult1.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include "mpi.h"
+#include <math.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+void print_results(char *prompt, int a[][], int row, int col, int id);
+
+int main(int argc, char **argv)
+{
+  int coreId, totalCore, counter,colSize, blockSize, dum, i, j;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &coreId);
+  MPI_Comm_size(MPI_COMM_WORLD, &totalCore);
+  colSize = 16*24;
+  blockSize = 16;
+  int tempA[colSize][blockSize]; //holds row of A
+  int tempB[blockSize][colSize]; //holds cols of B
+  int multiResult = [blockSize][blockSize]; //holds multiplication result 
+  int finalResultPerCore[colSize][blockSize]; //stores the final result done by each core.
+  if(coreId == 0){
+      int A[colSize][colSize];
+      int B[colSize][colSize];
+      for(i = 0; i< colSize; i++){
+          for(j=0; j<colSize; j++){
+              A[i][j] == 1;
+              B[i][j] == 2;
+          }
+      }
+  }
+  //sending the matrices from core 0 to the rest
+  for(counter =1; counter<totalCore; counter++ ){
+      if(coreId == 0){
+          dum = counter*blockSize; //16
+          //dividing matrix A into parcel
+          for(i = dum; i<dum + blockSize; i++){  //16 ~ 31
+            for(j = 0; j<colSize; j++){
+                tempA[i-dum][j] = A[i][j];
+            }
+          }
+          //dividing matrix B into parcel
+          for(i = dum; i<dum + blockSize; i++){  //16 ~ 31
+            for(j = 0; j<colSize; j++){
+                tempB[j][i-dum] = B[j][i];
+            }
+          }
+          MPI_Send(&tempA,blockSize*colSize,MPI_Int,counter,0,MPI_COMM_WORLD)
+          MPI_Send(&tempB,blockSize*colSize,MPI_Int,counter,1,MPI_COMM_WORLD)
+      }
+      if(coreId == counter){
+          MPI_Recv(&tempA, blockSize*colSize, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      }
+      if(coreId == counter){
+          MPI_Recv(&tempB, blockSize*colSize, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      }
+      if(counter == totalCore-1){
+          if(coreId == 0){
+              for(i=0; i<blockSize; i++){
+                  for(j = 0; j<colSize; j++){
+                      tempA[i][j] = A[i][j];
+                      tempB[j][i] = B[j][i];
+                  }
+              }
+          }
+      }
+  }
+    print_results("A temp = ", tempA, blockSize, colSize, counter);
+    print_results("B temp = ", tempB, colSize, blockSize, counter);
+    MPI_Finalize();
+
+}
+
+void print_results(char *prompt, int a[][], int row, int col, int id)
+{
+    int i, j;
+    printf ("\n\n On core: %d, %s\n",id, prompt);
+    for (i = 0; i < row; i++) {
+            for (j = 0; j < col; j++) {
+                    printf(" %d", a[i][j]);
+            }
+            printf ("\n");
+    }
+    printf ("\n\n");
+}
+
--- hw2.c
+++ hw2.c
@@ -2,35 +2,37 @@
 #include<stdio.h>
 int main(int argc,char*argv[]){
     int coreId, totalCore, i;
-    int messageSize = 100;
+    int messageSize = 100; //number of numbers to send, change this number to send message of different size
     int trial = 10;
     int message [messageSize];
     double total_my_bcast_time = 0.0;
     double total_mpi_bcast_time = 0.0;
+    //initialize the message
     for( i = 0; i<messageSize; i = i+1){
         message[i] =i;
     }
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &coreId);
     MPI_Comm_size(MPI_COMM_WORLD, &totalCore);
-    MPI_Request request;
-    int times;
+    //MPI_Request request;
+    int times; //a simple counter for the trials
     int anotherBuffer [messageSize];
-    for(times = 0; times < trial, times++){
+    for(times = 0; times < trial; times++){ //run 10 trials and collect the average
      //my_bcast
     MPI_Barrier(MPI_COMM_WORLD);
     total_my_bcast_time -= MPI_Wtime();
     if(coreId == 0){
          int count;
+         //send the message to the rest of the cores
          for(count = 1; count<totalCore; count = count +1){
             MPI_Send(message, messageSize, MPI_INT, count, 0, MPI_COMM_WORLD);
          }
-        printf("%d message broadcasted from core %d using Send \n", messageSize, coreId);
+        //printf("%d message broadcasted from core %d using Send \n", messageSize, coreId);
     }
     else{
+        //get message from core 0
         MPI_Recv(anotherBuffer, messageSize, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        printf("%d numbers received from core 0 by core %d using Recv\n", messageSize, coreId);  
-        anotherBuffer = NULL; 
+        //printf("%d numbers received from core 0 by core %d using Recv\n", messageSize, coreId);  
     }
     MPI_Barrier(MPI_COMM_WORLD);
     total_my_bcast_time += MPI_Wtime();
@@ -38,15 +40,18 @@ int main(int argc,char*argv[]){
     //MPI_Bcast 
      MPI_Barrier(MPI_COMM_WORLD);
      total_mpi_bcast_time -= MPI_Wtime();
+     //invoke mpi_bcast on all cores
      MPI_Bcast(message, messageSize, MPI_INT, 0, MPI_COMM_WORLD);
-     printf("%d numbers received from core 0 by core usng Bcast %d \n", messageSize, coreId);   
+     //printf("%d numbers received from core 0 by core usng Bcast %d \n", messageSize, coreId);   
      MPI_Barrier(MPI_COMM_WORLD);
      total_mpi_bcast_time += MPI_Wtime();
     }
 
      if (coreId == 1) {
      printf("Data size = %d\n", messageSize);
+     //mpi_bcast average speed of 10 trials
      printf("Average MPI_Bcast time = %lf\n", total_mpi_bcast_time/trial);
+     //my bcast speed of 10 trials
      printf("Average my bcast time = %lf\n", total_my_bcast_time/trial);
   }
     MPI_Finalize();
